67e13c146107a6ab030c14e40be636aac186e01a
Lenovo <staliarou@gmail.com>
2019-05-31 10:45:08 +0300

    commit

--- src/components/ItemsListAndFilterMenu.js
+++ src/components/ItemsListAndFilterMenu.js
@@ -16,12 +16,66 @@ class ItemsListAndFilterMenu extends React.Component {
         let priceFrom = filterData.price.from;
         let priceTo = filterData.price.to;
 
-        let filteredTypes = this.state.data.filter(item => {
+        isTypeMatching = () => {
+            alert('type')
+        };
+
+        isQueryMatching = () => {
+            alert('query')
+        };
+
+        isPriceMatching = () => {
+            alert('price')
+        };
+
+        return isTypeMatching() && isQueryMatching() && isPriceMatching();
+
+        /*let filteredTypesQuery, filteredTypesQueryAndPrice, storageOfFilteredTypesAndQuery;
+
+        if (priceFrom === null && types.length !== 0 || query !== '' ) {
+            filteredTypesQuery = this.state.data.filter(item => {
+                let isTypeMatching = types.indexOf(item.type) !== -1;
+                let isQueryMatching = query.length > 0 && item.title.toLowerCase().indexOf(query) !== -1;
+                return isQueryMatching || isTypeMatching;
+            });
+            this.setState({filteredData: filteredTypesQuery})
+
+        } else if (types.length === 0 && query === '' && priceFrom === null) {
+            filteredTypesQuery = this.state.data.filter(item => {
+                return true;
+            });
+            this.setState({filteredData: filteredTypesQuery})
+
+        } else if (types.length === 0 && query === '' && priceFrom !== null) {
+            filteredTypesQuery = this.state.data.filter(item => {
+                let isPriceFromMatching = item.priceNum >= priceFrom;
+                return isPriceFromMatching;
+            });
+            this.setState({filteredData: filteredTypesQuery})
+
+        } else if (priceFrom !== null && types.length !== 0 || query !== '') {
+            filteredTypesQuery = this.state.data.filter(item => {
+                let isTypeMatching = types.indexOf(item.type) !== -1;
+                let isQueryMatching = query.length > 0 && item.title.toLowerCase().indexOf(query) !== -1;
+                return isQueryMatching || isTypeMatching;
+            });
+            storageOfFilteredTypesAndQuery = filteredTypesQuery;
+            filteredTypesQueryAndPrice = storageOfFilteredTypesAndQuery.filter(item => {
+                let isPriceFromMatching = item.priceNum >= priceFrom;
+                return isPriceFromMatching;
+            });
+            this.setState({filteredData: filteredTypesQueryAndPrice})
+        }*/
+
+
+       /* let filteredTypes = this.state.data.filter(item => {
             if (types.length === 0 && query === '' && priceFrom === null) {
                 return true
             }
             let isTypeMatching = types.indexOf(item.type) !== -1;
             let isQueryMatching = query.length > 0 && item.title.toLowerCase().indexOf(query) !== -1;
+
+
             let isPriceFromMatching;
             if (priceFrom === null) {
                 isPriceFromMatching = false;
@@ -30,9 +84,23 @@ class ItemsListAndFilterMenu extends React.Component {
             }
             //let isPriceToMatching = priceTo <= item.priceNum;
 
-            return isQueryMatching || isTypeMatching || isPriceFromMatching;
+            if (types.length === 0 && query === '') {
+                return isPriceFromMatching;
+            }
+
+            return isQueryMatching || isTypeMatching;
         });
+
         this.setState({filteredData: filteredTypes})
+
+        if (types.length !== 0 || query !== '') {
+            this.setState({filteredData: filteredTypes})
+        } else {
+            let filteredTypes2 = filteredTypes.filter(item => {
+
+            })
+        }*/
+
     };
 
     render() {
