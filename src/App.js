2f60facb57a0fa49a2995d4578922a0c00f4a675
Lenovo <staliarou@gmail.com>
2020-11-30 11:46:11 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ class App extends Component {
             <ItemsListAndFilterMenu overlaySwitcher={this.overlaySwitcher} />
         </React.Fragment>
 
-
+//
         
       /*<div className="App">
         <header className="App-header">
