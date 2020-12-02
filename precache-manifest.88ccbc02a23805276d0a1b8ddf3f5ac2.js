ede723c60c7f0b6632e167da090b8e5dfc8fc5ff
Lenovo <staliarou@gmail.com>
2020-12-02 15:57:21 +0300

    Updates

---
+++ precache-manifest.88ccbc02a23805276d0a1b8ddf3f5ac2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f45af86e35da5520ec2a",
+    "url": "/React-goods-app/static/css/main.2f18887f.chunk.css"
+  },
+  {
+    "revision": "f45af86e35da5520ec2a",
+    "url": "/React-goods-app/static/js/main.f45af86e.chunk.js"
+  },
+  {
+    "revision": "35d4967322379881d30c",
+    "url": "/React-goods-app/static/js/1.35d49673.chunk.js"
+  },
+  {
+    "revision": "71df4a0b47da266f9fb9",
+    "url": "/React-goods-app/static/js/runtime~main.71df4a0b.js"
+  },
+  {
+    "revision": "143eca6122f9357415f5e72f80f33fd1",
+    "url": "/React-goods-app/index.html"
+  }
+];
